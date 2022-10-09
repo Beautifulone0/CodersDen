ba68fad8da6c8df237081745515d4162f6343ad7
beautifulone0 <konyeanifaith@gmail.com>
2022-10-09 21:51:46 +0100

    CodersDen

---
+++ Week 1/index.js
@@ -0,0 +1,32 @@
+//Question 1 Write a single line comment which says, comments can make code readable
+  //comments can make code readable
+//Question 2 Write another single comment which says, Welcome to 30DaysOfJavaScript
+  //Welcome to 30DaysOfJavaScript
+//Question 3 Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+  /*comments can make code readable, easy to reuse and informative */
+//Question 4 Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+  //Answer is in the variable js file
+//Question 5 Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+  //Answer is in the datatypes.js file
+//Question 6 Declare four variables without assigning values
+let nickname
+let state
+let job
+let capitalcity
+//Qustion 7 Declare four variables with assigned values
+let name = 'beautifulone'
+let city = 'lagos state nigeria'
+let year = 2022
+let ismarried = false
+//Question 8 Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstname1 = 'Awele'
+let lastname1 = 'beautifulone'
+let issingle1 = true
+let country1 = 'nigeria'
+let age1 = 22
+//Question 9 Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstname = 'Awele' ,lastname = 'beautifulone' ,issingle = true ,country = 'nigeria' ,age = 22
+//Question 10 Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 22
+let yourAge = 25
+console.log(myAge+" " +yourAge)
---
+++ Week 1/variables.js
@@ -0,0 +1,5 @@
+//Question 4 Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let firstname = 'beautifulone'
+let issingle = true
+let age;
+let emptyvalue = null
