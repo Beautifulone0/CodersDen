0cd4f1e7991d569df828305ee33b72a1a00718ce
beautifulone0 <konyeanifaith@gmail.com>
2022-10-11 18:10:07 +0100

    week 2 excersises

---
+++ Week 2/level 3.js
@@ -0,0 +1,6 @@
+//Question 1 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+       let number = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+       console.log(number.match(/Love/gi))
+//Question 2 Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+       let string = 'You cannot end a sentence with because because because is a conjunction'
+       console.log(string.match(/because/g))
